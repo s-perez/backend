9ef3b183d0a035095b422f195ebf909d8e8dc0ae
Salvador Pérez <sisekeom@gmail.com>
2015-05-07 20:48:16 +0200

    Change the User ForeignKey(unique=True) to a OneToOneField. A warning when executing the makemigrations advised to do so due to a performance improvement

--- Authentication/models.py
+++ Authentication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserAccount(models.Model):
-    user = models.ForeignKey(User, unique=True)
+    user = models.OneToOneField(User)
     real_name = models.CharField(max_length=200)
     country = models.CharField(max_length=210)
     phone = models.IntegerField(blank=True)
